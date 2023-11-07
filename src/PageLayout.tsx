c1d6dd6d9925a75819e8f99b23938be287d0ea45
Justin Mancusi <jmancusi@yext.com>
2023-11-06 22:18:45 -0500

    initial commit

---
+++ src/PageLayout.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export const PageLayout = () => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+      </div>
+    </div>
+  );
+};
